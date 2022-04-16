5b6e186f2e1847dd971dd8759c5fef40c82d1fb1
FollowerOfBigboss <followerofbigboss@gmail.com>
2022-04-16 05:38:35 +0300

    Add border to indicate canvas region in first sample

--- src/1.getting_started/1.1/index.js
+++ src/1.getting_started/1.1/index.js
@@ -12,10 +12,7 @@ function main() {
 	{
 		requestAnimationFrame(render);
 	}
-	requestAnimationFrame(render);
-		
-//	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-//	gl.clear(gl.COLOR_BUFFER_BIT);
+	requestAnimationFrame(render);		
 }
 
 window.onload = main;

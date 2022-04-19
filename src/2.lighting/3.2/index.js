b797f9b83874b9680c3baa96dfb19adbe69c4643
FollowerOfBigboss <followerofbigboss@gmail.com>
2022-04-20 01:44:23 +0300

    Add material exercise

---
+++ src/2.lighting/3.2/index.js
@@ -0,0 +1,216 @@
+import {Shader} from './../../helpers/shader.js';
+import {Camera, Camera_Movement} from './../../helpers/camera.js';
+import './../../helpers/gl-matrix-min.js';
+
+function main() {
+	const canvas = document.querySelector("#glctx");
+	const gl = canvas.getContext("webgl2", { alpha: false });
+	
+	if (gl == null)
+	{
+		alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+		return;
+	}
+
+	var camera = new Camera();
+	camera.init_1(glMatrix.vec3.fromValues(0.0, 0.0, 3.0));
+	
+	var deltaTime = 0.0;
+	var lastFrame = 0.0;
+ 
+	const vertices = [
+		-0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+		 0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+		 0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+		 0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+		-0.5,  0.5, -0.5,  0.0,  0.0, -1.0,
+		-0.5, -0.5, -0.5,  0.0,  0.0, -1.0,
+
+		-0.5, -0.5,  0.5,  0.0,  0.0,  1.0,
+		 0.5, -0.5,  0.5,  0.0,  0.0,  1.0,
+		 0.5,  0.5,  0.5,  0.0,  0.0,  1.0,
+		 0.5,  0.5,  0.5,  0.0,  0.0,  1.0,
+		-0.5,  0.5,  0.5,  0.0,  0.0,  1.0,
+		-0.5, -0.5,  0.5,  0.0,  0.0,  1.0,
+
+		-0.5,  0.5,  0.5, -1.0,  0.0,  0.0,
+		-0.5,  0.5, -0.5, -1.0,  0.0,  0.0,
+		-0.5, -0.5, -0.5, -1.0,  0.0,  0.0,
+		-0.5, -0.5, -0.5, -1.0,  0.0,  0.0,
+		-0.5, -0.5,  0.5, -1.0,  0.0,  0.0,
+		-0.5,  0.5,  0.5, -1.0,  0.0,  0.0,
+
+		 0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+		 0.5,  0.5, -0.5,  1.0,  0.0,  0.0,
+		 0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+		 0.5, -0.5, -0.5,  1.0,  0.0,  0.0,
+		 0.5, -0.5,  0.5,  1.0,  0.0,  0.0,
+		 0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+
+		-0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+		 0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+		 0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+		 0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+		-0.5, -0.5,  0.5,  0.0, -1.0,  0.0,
+		-0.5, -0.5, -0.5,  0.0, -1.0,  0.0,
+
+		-0.5,  0.5, -0.5,  0.0,  1.0,  0.0,
+		 0.5,  0.5, -0.5,  0.0,  1.0,  0.0,
+		 0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+		 0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+		-0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
+		-0.5,  0.5, -0.5,  0.0,  1.0,  0.0
+	];
+
+	var mouseLocked = false;
+	// https://www.gavsblog.com/blog/detect-single-and-multiple-keypress-events-javascript
+	let keysPressed = {};
+	document.addEventListener('keydown', (event) => {
+                keysPressed[event.key] = true;
+	});
+	document.addEventListener('keyup', (event) => {
+                keysPressed[event.key] = false;
+	});
+
+	// pointerlock things
+	const pointerlockchangeexist = ("onpointerlockchange" in document)==true;
+	if (pointerlockchangeexist) {
+		document.addEventListener('pointerlockchange', () => {
+			if(document.pointerLockElement === canvas) {
+				mouseLocked = true;
+    				// console.log('The pointer lock status is now locked');
+  			} else {
+				mouseLocked = false;
+    				// console.log('The pointer lock status is now unlocked');
+  			}
+		}, false);
+        }
+	document.addEventListener('pointerlockerror', () => { 
+		// console.log('Pointerlock failed!');
+		mouseLocked = false; 
+	}, false);
+	
+	canvas.onclick = function(event) {
+		const rect = canvas.getBoundingClientRect();
+		const x = event.clientX - rect.left;
+		const y = event.clientY - rect.top;
+		canvas.requestPointerLock();
+		mouseLocked = true;	
+	};
+	
+	canvas.addEventListener('wheel', (event) => {
+		const wheelDelta = event.deltaY > 0 ? -1 : 1;
+		camera.ProcessMouseScroll(wheelDelta);
+	});
+
+	document.addEventListener("mousemove", (event) => {	
+		// No lock no processing
+		if (mouseLocked != true)
+			return;
+		
+		let xoffset = event.movementX;
+		// Revert y axis
+		let yoffset = -event.movementY;		
+		camera.ProcessMouseMovement(xoffset, yoffset);
+		
+	}, false);
+    
+	function processInput()
+	{	
+		if (keysPressed['w'] === true) { 
+			camera.ProcessKeyboard(Camera_Movement.FORWARD, deltaTime);	
+		}
+		if (keysPressed['s'] === true) { 
+			camera.ProcessKeyboard(Camera_Movement.BACKWARD, deltaTime);
+		}
+		if (keysPressed['a'] === true) {
+			camera.ProcessKeyboard(Camera_Movement.LEFT, deltaTime);
+		}
+		if (keysPressed['d'] === true) {
+			camera.ProcessKeyboard(Camera_Movement.RIGHT, deltaTime);
+		}
+	}
+
+
+	gl.enable(gl.DEPTH_TEST);
+	var lightingShader  = new Shader(gl, "materials_vs", "materials_fs");
+	var lightCubeShader = new Shader(gl, "light_cube_vs", "light_cube_fs");
+
+	var cubeVAO = gl.createVertexArray();
+	var VBO = gl.createBuffer();
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);		
+
+	gl.bindVertexArray(cubeVAO);
+
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
+	gl.enableVertexAttribArray(0);
+
+	gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3*Float32Array.BYTES_PER_ELEMENT);
+	gl.enableVertexAttribArray(1);
+	
+	var lightCubeVAO = gl.createVertexArray();
+	gl.bindVertexArray(lightCubeVAO);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, VBO);	
+
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
+	gl.enableVertexAttribArray(0);
+
+
+	var lightPos = glMatrix.vec3.fromValues(1.2, 1.0, 2.0);
+	function render(now)
+	{
+		const currentFrame = Date.now() / 1000;
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput();
+
+		gl.clearColor(0.1, 0.1, 0.1, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+		// be sure to activate shader when setting uniforms/drawing objects
+		lightingShader.use();
+		lightingShader.setVec3v("light.position", lightPos);
+		lightingShader.setVec3v("viewPos", camera.Position);
+			
+		lightingShader.setVec3("light.ambient", 1.0, 1.0, 1.0); // note that all light colors are set at full     intensity
+		lightingShader.setVec3("light.diffuse", 1.0, 1.0, 1.0);
+		lightingShader.setVec3("light.specular", 1.0, 1.0, 1.0);
+
+		// material properties
+		lightingShader.setVec3("material.ambient", 0.0, 0.1, 0.06);
+		lightingShader.setVec3("material.diffuse", 0.0, 0.50980392, 0.50980392);
+		lightingShader.setVec3("material.specular", 0.50196078, 0.50196078, 0.50196078);
+		lightingShader.setFloat("material.shininess", 32.0);
+	
+	
+		let projection = glMatrix.mat4.create();
+		glMatrix.mat4.perspective(projection, glMatrix.glMatrix.toRadian(camera.Zoom), 800/600, 0.1, 100.0);	
+		let view = camera.GetViewMatrix();
+		lightingShader.setMat4("projection", projection);
+		lightingShader.setMat4("view", view);
+		
+		let model = glMatrix.mat4.create();
+		lightingShader.setMat4("model", model);
+
+		gl.bindVertexArray(cubeVAO);
+		gl.drawArrays(gl.TRIANGLES, 0, 36);
+	
+		lightCubeShader.use();
+		lightCubeShader.setMat4("projection", projection);
+		lightCubeShader.setMat4("view", view);	
+		glMatrix.mat4.translate(model, model, lightPos);
+		glMatrix.mat4.scale(model, model, glMatrix.vec3.fromValues(0.2, 0.2, 0.2));
+		lightCubeShader.setMat4("model", model);	
+		gl.bindVertexArray(lightCubeVAO);
+		gl.drawArrays(gl.TRIANGLES, 0, 36);
+
+		requestAnimationFrame(render);
+	}
+	requestAnimationFrame(render);
+}
+
+main();
